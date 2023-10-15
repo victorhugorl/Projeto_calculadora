c683fb1b2d2fab99b368bda6aa1ad757c7932edb
Victor Hugo Rangel <victorlimasg@outlook.com>
2023-10-15 11:56:05 -0300

    adicionando label e corrigindo bugs na calculadora

--- src/main.py
+++ src/main.py
@@ -1,12 +1,30 @@
 import sys
 
-from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel)
-
+from display import Display
 from main_window import MainWindow
-
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication
+from variables import WINDOW_ICON_PATH
 if __name__ == '__main__':
+    # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
+    # Define o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
+    if sys.platform.startswith('win'):
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+            u'CompanyName.ProductName.SubProduct.VersionInformation')
+        # Arbitrary string
+
+    # Display
+    display = Display()
+    window.addToVLayout(display)
+
+    # Executa tudo
+    window.adjustFixedSize()
     window.show()
     app.exec()
--- src/main_window.py
+++ src/main_window.py
@@ -1,17 +1,21 @@
-import PySide6.QtCore
-from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel)
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
-
+        # Configurando o layout básico
         self.cw = QWidget()
-        self.v_layout = QVBoxLayout()
-        self.cw.setLayout(self.v_layout)
-
-        self.label1 = QLabel('O meu texto')
-        self.v_layout.addWidget(self.label1)
-
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
         self.setCentralWidget(self.cw)
+        # Título da janela
+        self.setWindowTitle('Calculadora')
+
+    def adjustFixedSize(self):
+        # Última coisa a ser feita
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
+
+    def addToVLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)

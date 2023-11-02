b9ee5c3e9b84bdcaf2407ae064f177668ac0b77d
Victor Hugo Rangel <victorlimasg@outlook.com>
2023-11-02 17:47:59 -0300

    creating module info.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,22 @@
 import sys
 
 from display import Display
+from info import Info
 from main_window import MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 from variables import WINDOW_ICON_PATH
+
+
 if __name__ == '__main__':
     # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    # Info
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addToVLayout(info)
+
     # Define o ícone
     icon = QIcon(str(WINDOW_ICON_PATH))
     window.setWindowIcon(icon)
